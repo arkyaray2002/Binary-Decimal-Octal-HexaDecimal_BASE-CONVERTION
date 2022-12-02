176a5632e0fb5e3afbe37823c47de279a86371dd
arkyaray2002 <113771514+arkyaray2002@users.noreply.github.com>
2022-12-02 22:32:05 +0530

    Add files via upload

---
+++ base-converter.cpp
@@ -0,0 +1,131 @@
+#include <iostream> 
+#include <cstdio>
+#include <cmath>
+using namespace std; 
+
+Any2Bin(int number, int initial_base)
+{
+	int a[10],i;
+	for(i=0;number>0;i++)    
+	{    
+		a[i] = number%2;    
+		number = number/2;    
+	}
+	printf("\n THE NUMBER IN BASE 2 IS : ");
+	for(i=i-1;i>=0;i--)    
+	{    
+		printf("%d",a[i]);    
+	}
+}
+
+
+Bin2Any(int number, int final_base)
+{
+	int x=0, base, rem;
+	while (number > 0)  
+    {  
+        rem = number % 10;
+        x = x + rem * base;
+        number = number / 10;
+        base = base * 2;
+    }
+			if(final_base == 8)
+			{
+				printf("THE NUMBER IN BASE %d IS : %o" ,final_base ,x);
+			}
+			if(final_base == 10)
+			{
+				printf("THE NUMBER IN BASE %d IS : %d" ,final_base ,x);
+			}
+			if(final_base == 16)
+			{
+				printf("THE NUMBER IN BASE %d IS : %x" ,final_base ,x);
+			}
+}
+
+
+int main()
+{
+	int initial_base, final_base, number;
+	cout<< "ENTER THE CURRENT BASE OF THE NUMBER : \t";
+	cin>> initial_base;
+	cout<< "ENTER THE FINAL BASE OF THE NUMBER : \t";
+	cin>> final_base;
+	
+		if(initial_base == 2)
+		{
+			printf("ENTER THE NUMBER TO BE CONVERTED : \t");
+			scanf("%d" ,&number);
+			Bin2Any(number,final_base);
+		}
+		
+		
+		if(initial_base == 8)
+		{
+		printf("ENTER THE NUMBER TO BE CONVERTED : \t");
+		scanf("%o" ,&number);
+			if(final_base == 2)
+			{
+				Any2Bin(number,initial_base);
+			}
+			if(final_base == 10)
+			{
+				printf("THE NUMBER IN BASE %d IS : %d" ,final_base ,number);
+			}
+			if(final_base == 16)
+			{
+				printf("THE NUMBER IN BASE %d IS : %x" ,final_base ,number);
+			}
+			if(final_base != 16 && final_base != 10 && final_base != 2)
+			{
+				printf("\n WRONG CHOICE \n ENTER DATA CORRECTLY");
+			}	
+		}
+
+	
+		if(initial_base == 10)
+		{
+		printf("ENTER THE NUMBER TO BE CONVERTED : \t");
+		scanf("%d" ,&number);
+			if(final_base == 2)
+			{
+				Any2Bin(number,initial_base);
+			}
+			if(final_base == 8)
+			{
+				printf("THE NUMBER IN BASE %d IS : %o" ,final_base ,number);
+			}
+			if(final_base == 16)
+			{
+				printf("THE NUMBER IN BASE %d IS : %x" ,final_base ,number);
+			}
+			if(final_base != 16 && final_base != 8 && final_base != 2)
+			{
+				printf("\n WRONG CHOICE \n ENTER DATA CORRECTLY");
+			}	
+		}
+
+
+		if(initial_base == 16)
+		{
+		printf("ENTER THE NUMBER TO BE CONVERTED : \t");
+		scanf("%x" ,&number);
+			if(final_base == 2)
+			{
+				Any2Bin(number,initial_base);
+			}
+			if(final_base == 8)
+			{
+				printf("THE NUMBER IN BASE %d IS : %o" ,final_base ,number);
+			}
+			if(final_base == 10)
+			{
+				printf("THE NUMBER IN BASE %d IS : %d" ,final_base ,number);
+			}
+			if(final_base != 10 && final_base != 8 && final_base != 2)
+			{
+				printf("\n WRONG CHOICE \n ENTER DATA CORRECTLY");
+			}	
+		}
+
+}
